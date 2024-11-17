54dc5e58c5eceb9a345fdcbf42c319aeb88d22a1
Sinead-dot <sinead.moodie@gmail.com>
2024-11-17 16:27:11 +0000

    Fix API routing

--- api/hello.py
+++ api/hello.py
@@ -1,5 +1,14 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Hello from Python!"
-    }
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+        message = {
+            "message": "Hello from Carbon Footprint Analyzer!"
+        }
+        
+        self.wfile.write(str(message).encode())
+        return
