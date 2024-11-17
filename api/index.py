b1cf013488958f519f116916da597f58bebefe37
Sinead-dot <sinead.moodie@gmail.com>
2024-11-17 16:57:19 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,15 @@
-{
-    def handler(request):
-    return {
-        "statusCode": 200,
-        "body": {
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+        message = {
             "message": "Hello from Carbon Footprint Analyzer!"
         }
-    }
+        
+        self.wfile.write(json.dumps(message).encode())
+        return
